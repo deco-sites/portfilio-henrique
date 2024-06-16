cdf8b4ea3f8d47fe03d5cbc3bbf6c0b649d9fd3d
HenriqueeAl <henrifulls@gmail.com>
2024-06-16 18:08:52 -0300

    changes hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,8 +3,25 @@ interface Props {
   * @description The description of name.
   */
   name?: string;
+  office?: string;
+  linkedin: string;
 }
 
-export default function Section({ name = "Capy" }: Props) {
-  return <div>Hello {name}</div>
+export default function Section({ name = "Henrique Almeida", office = "Desenvolvedor", linkedin }: Props) {
+  return (
+    <div class="w-[100vw] h-[100vh]">
+      <div>
+        <h1>{name}</h1>
+        <h3>{office}</h3>
+        <div>
+            <div>
+              <a href={linkedin}></a>
+            </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
 }
