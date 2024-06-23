88cf46f3e9093eb032266bb5d4ec1438ecd9bd05
HenriqueeAl <henrifulls@gmail.com>
2024-06-22 22:42:59 -0300

    projects feat, concat init

--- sections/Concat.tsx
+++ sections/Concat.tsx
@@ -6,5 +6,23 @@ interface Props {
 }
 
 export default function Section({ name = "Capy" }: Props) {
-  return <div>Hello {name}</div>
+  return (
+    <div class="w-[80%] h-[100vh] flex items-center max-w-[1200px] m-auto">
+      <form class="w-[80%] bg-[#2F3B48] m-auto p-10 rounded-lg">
+        <h3 class="text-white text-2xl font-black">Entre em contato</h3>
+        <div class="flex flex-col font-black mt-5">
+          <label class="text-white text-base mb-1 ml-1">Nome</label>
+          <input class="p-5 bg-[#1F2B38] w-[40%] rounded-lg outline-none placeholder:text-[#FFFFFF33] text-[#FFFFFF]" type="text" placeholder="Digite aqui ..." />
+        </div>
+        <div class="flex flex-col font-black mt-5">
+          <label class="text-white text-base mb-1 ml-1">Numero</label>
+          <input class="p-5 bg-[#1F2B38] w-[40%] rounded-lg outline-none placeholder:text-[#FFFFFF33] text-[#FFFFFF]" type="text" placeholder="Digite aqui ..." />
+        </div>
+        <div class="flex flex-col font-black mt-5">
+          <label class="text-white text-base mb-1 ml-1">Email</label>
+          <input class="p-5 bg-[#1F2B38] w-[40%] rounded-lg outline-none placeholder:text-[#FFFFFF33] text-[#FFFFFF]" type="text" placeholder="Digite aqui ..." />
+        </div>
+      </form>
+    </div>
+  )
 }
--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -4,15 +4,26 @@ interface Props {
   /**
   * @description The description of name.
   */
+  name: string;
   projects: Array<{ name: string; link: string ; image: ImageWidget}>;
 }
 
-export default function Section({ projects }: Props) {
+const items = [{
+  name: 'teste',
+  link: 'teste',
+  image: 'teste',
+}]
+
+export default function Section({ name = "Projetos", projects = items }: Props) {
   return (
     <div class="w-[80%] h-[100vh] max-w-[1200px] m-auto">
-      <h3 class="text-white text-2xl font-black">Projetos</h3>
-      <div class="flex">
+      <h3 class="text-white text-2xl font-black">{name}</h3>
+      <div class="flex justify-between flex-wrap mt-[70px]">
+          {projects.map((item)=> (
+            <a href={item.link} class="mb-[70px] w-[30%] h-60 bg-no-repeat bg-contain cursor-pointer rounded-lg" style={`background-image: url(${item.image})`} target="_blank">
 
+            </a>
+          ))}
       </div>
     </div>
   )
